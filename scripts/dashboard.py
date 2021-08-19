8a1344c2b08c7b2966eed5055c724ce174e367dd
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-08-18 18:06:09 -0600

    add requirements

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
-import streamlit
+import streamlit as st
 from sklearn.linear_model import LinearRegression
 
 sns.set_theme()
